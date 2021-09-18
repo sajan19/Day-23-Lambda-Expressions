20061cbe7acf7dd31770bfade2cb207dca755efb
Sajan <sajanmhatre29@gmail.com>
2021-09-19 02:09:45 +0530

    Lambda Function to show result

--- src/MathOperationApp.java
+++ src/MathOperationApp.java
@@ -2,6 +2,9 @@ public class MathOperationApp {
    @FunctionalInterface
     interface IMathFunction{
         int calculate(int a, int b);
+        static void printResult(int a, int b, String function , IMathFunction fobj){
+            System.out.println("Result of "+ function+" is "+fobj.calculate(a, b));
+        }
     }
     public static void main(String[] args) {
         System.out.println("Welcome to Lambda Expressions");
@@ -9,9 +12,17 @@ public class MathOperationApp {
         IMathFunction add= Integer::sum;
         IMathFunction multiply = (x,y)-> x*y;
         IMathFunction divide = (int x, int y)-> x/y;
+        IMathFunction subtract = (int x, int y)-> x/y;
         //Add, Multiply and Divide Two Numbers using Lambda Expressions
         System.out.println("Addition of Two Numbers is: "+ add.calculate(23,67));
+        System.out.println("Subtraction of Two Numbers is: "+ subtract.calculate(123,67));
         System.out.println("Multipication of Two Numbers is: "+ multiply.calculate(23,67));
-        System.out.println("Division of Two Numbers is: "+ divide.calculate(23,67));
+        System.out.println("Division of Two Numbers is: "+ divide.calculate(323,67));
+
+        //Passing Lambda Expressions as Function Parameter to Print Result using Static Function
+        IMathFunction.printResult(34,56, "Addition", add);
+        IMathFunction.printResult(134,56, "Subtraction", subtract);
+        IMathFunction.printResult(34,56, "Multiplication", multiply);
+        IMathFunction.printResult(134,56, "Division", divide);
     }
 }
